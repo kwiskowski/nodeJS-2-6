866328363fdb038b905ebc10026d445f4aefa68e
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2024-09-15 01:14:53 +0200

    Update schemas.js

--- service/schemas.js
+++ service/schemas.js
@@ -18,6 +18,28 @@ const contact = new Schema({
   },
 });
 
-const Contact = mongoose.model("contacts", contact);
+const user = new Schema({
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+});
 
-module.exports = Contact;
+const Contact = mongoose.model("contacts", contact);
+const User = mongoose.model("users", user);
+
+module.exports = { Contact, User };
